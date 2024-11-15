199eeaedbcca3c7397d7b1e4e651136dc9c849a1
Srikanth1316 <srikantharjun30@gmail.com>
2024-11-15 11:51:32 +0530

    Update predictor.py

--- predictor.py
+++ predictor.py
@@ -4,7 +4,7 @@ with open('model_pickle2', 'rb') as f;
 model = pickle.load(f)
 
 
-def predict_house_price(area,bathrooms,age)
-input_data =[[area,bathrooms,age]]
+def predict_house_price(area,bedrooms,age)
+input_data =[[area,bedrooms,age]]
 predicted_price = model.predict(input_data)
 return predicted_price[0]
